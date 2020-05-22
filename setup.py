ab362d03dedb3dd5120e15aa02866ffd2bc744d7
Rajas Sambhare <rajassambhare@nuralogix.ai>
2020-05-22 12:43:02 -0400

    Changed package name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='dfxapi',
+    name='dfx-apiv2-client',
     version='0.0.1',
     packages=['dfx_apiv2_client'],
     install_requires=['aiohttp[speedups]'],
     setup_requires=['wheel'],
-    description='dfx_apiv2_client is an async client for the DeepAffex API.',
+    description='dfx-apiv2-client is an async client for the DeepAffex API.',
     entry_points={
         'console_scripts': [
             'apiexample = apiexample:cmdline',
