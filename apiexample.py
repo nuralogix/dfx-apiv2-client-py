bd85a84978004f0c847010174936107b603baa8f
Rajas Sambhare <rajassambhare@nuralogix.ai>
2020-06-01 14:45:19 -0400

    Working REST add_data

--- apiexample.py
+++ apiexample.py
@@ -15,22 +15,7 @@ async def main(args):
     # dfxapi.Settings.rest_url = "https://api2.api.deepaffex.ai:9443"
 
     # Load creds
-    creds = {
-        "device_id": "",
-        "device_token": "",
-        "role_id": "",
-        "user_id": "",
-        "user_token": "",
-    }
-    if os.path.isfile(args.creds_file):
-        with open(args.creds_file, "r") as c:
-            creds = json.loads(c.read())
-
-    dfxapi.Settings.device_id = creds["device_id"]
-    dfxapi.Settings.device_token = creds["device_token"]
-    dfxapi.Settings.role_id = creds["role_id"]
-    dfxapi.Settings.role_id = creds["role_id"]
-    dfxapi.Settings.user_token = creds["user_token"]
+    creds = load_creds(args.creds_file)
 
     # Check API status
     async with aiohttp.ClientSession(raise_for_status=True) as session:
@@ -48,7 +33,7 @@ async def main(args):
             success = await register(creds, args.creds_file, args.license_key)
 
         if success:
-            update_creds_file(creds, args.creds_file)
+            save_creds(creds, args.creds_file)
         return
 
     # Login or logout
@@ -59,10 +44,10 @@ async def main(args):
             success = await login(creds, args.creds_file, args.email, args.password)
 
         if success:
-            update_creds_file(creds, args.creds_file)
+            save_creds(creds, args.creds_file)
         return
 
-    # Make sure we are registered and/or logged in
+    # The commands below need a token, so make sure we are registered and/or logged in
     if not dfxapi.Settings.device_token and not dfxapi.Settings.user_token:
         print("Please register and/or login first to obtain a token")
         return
@@ -87,51 +72,68 @@ async def main(args):
                 pretty_print(list_of_measurements)
             return
 
-    assert args.command == "measure" and args.subcommand == "make"
     # Make a measurement
-    # Read the files
+    assert args.command == "measure" and args.subcommand == "make"
+
+    # Verify preconditions
     payload_files = sorted(glob.glob(os.path.join(args.payloads_folder, "payload*.bin")))
     meta_files = sorted(glob.glob(os.path.join(args.payloads_folder, "metadata*.bin")))
     prop_files = sorted(glob.glob(os.path.join(args.payloads_folder, "properties*.json")))
-    number_chunks = min(len(payload_files), len(meta_files), len(prop_files))
-
-    if number_chunks <= 0:
+    number_files = min(len(payload_files), len(meta_files), len(prop_files))
+    if number_files <= 0:
         print(f"No payload files found in {args.payloads_folder}")
         return
+    with open(prop_files[0], 'r') as pr:
+        props = json.load(pr)
+        number_chunks_pr = props["number_chunks"]
+        duration_pr = props["duration_s"]
+    if number_chunks_pr != number_files:
+        print(f"Number of chunks in properties.json {number_chunks_pr} != Number of payload files {number_files}")
+        return
+    if duration_pr * number_chunks_pr > 120:
+        print(f"Total payload duration {duration_pr * number_chunks_pr} seconds is more than 120 seconds")
+        return
 
-    token = dfxapi.Settings.user_token if dfxapi.Settings.user_token else dfxapi.Settings.device_token
-    headers = {"Authorization": f"Bearer {token}"}
+    use_websocket = not args.rest
     async with aiohttp.ClientSession(headers=headers, raise_for_status=True) as session:
         # Create a measurement
-        measurementID = await dfxapi.Measurements.create(session, args.study_id)
-        print(f"Created measurement {measurementID}")
+        measurement_id = await dfxapi.Measurements.create(session, args.study_id)
+        print(f"Created measurement {measurement_id}")
 
-        # Iterate through the payload files and send chunks
-        for payload_file, meta_file, prop_file in zip(payload_files, meta_files, prop_files):
-            with open(payload_file, 'rb') as p, open(meta_file, 'rb') as m, open(prop_file, 'r') as pr:
-                payload_bytes = p.read()
-                meta_bytes = m.read()
-                props = json.load(pr)
+        measurement_files = zip(payload_files, meta_files, prop_files)
+        # Add data to the measurement
+        if use_websocket:
+            # Make a measurement using WebSocket
+            await measure_websocket(session, measurement_id, measurement_files)
+        else:
+            # Make a measurement using REST (no results are returned)
+            await measure_rest(session, measurement_id, measurement_files)
 
-                # Determine action
-                action = 'CHUNK::PROCESS'
-                if props["chunk_number"] == 0 and props["number_chunks"] > 1:
-                    action = 'FIRST::PROCESS'
-                elif props["chunk_number"] == props["number_chunks"] - 1:
-                    action = 'LAST::PROCESS'
-
-                chunkID = await dfxapi.Measurements.add_data(session, measurementID, props["chunk_number"], action,
-                                                             props["start_time_s"], props["end_time_s"], meta_bytes,
-                                                             payload_bytes)
-                print(f"Sent chunk #{chunkID} ({action}) ...waiting {props['duration_s']:.0f} seconds...")
-                # Sleep to simulate a live measurement and not hit the rate limit
-                await asyncio.sleep(props["duration_s"])
-
-        # Retrieve results
-        print("Measurement complete")
+        print(f"Measurement {measurement_id} complete")
 
 
-def update_creds_file(creds, creds_file):
+def load_creds(creds_file):
+    creds = {
+        "device_id": "",
+        "device_token": "",
+        "role_id": "",
+        "user_id": "",
+        "user_token": "",
+    }
+    if os.path.isfile(creds_file):
+        with open(creds_file, "r") as c:
+            creds = json.loads(c.read())
+
+    dfxapi.Settings.device_id = creds["device_id"]
+    dfxapi.Settings.device_token = creds["device_token"]
+    dfxapi.Settings.role_id = creds["role_id"]
+    dfxapi.Settings.role_id = creds["role_id"]
+    dfxapi.Settings.user_token = creds["user_token"]
+
+    return creds
+
+
+def save_creds(creds, creds_file):
     with open(creds_file, "w") as c:
         c.write(json.dumps(creds, indent=4))
         print(f"Credentials updated in {creds_file}")
@@ -139,6 +141,15 @@ def update_creds_file(creds, creds_file):
 
 def rand_reqid():
     return "".join(random.choices(string.ascii_letters, k=10))
+
+
+def determine_action(chunk_number, number_chunks):
+    action = 'CHUNK::PROCESS'
+    if chunk_number == 0 and number_chunks > 1:
+        action = 'FIRST::PROCESS'
+    elif chunk_number == number_chunks - 1:
+        action = 'LAST::PROCESS'
+    return action
 
 
 def pretty_print(list_of_dicts):
@@ -205,13 +216,36 @@ def logout(creds, creds_file):
     return True
 
 
+async def measure_rest(session, measurement_id, measurement_files):
+    for payload_file, meta_file, prop_file in measurement_files:
+        with open(payload_file, 'rb') as p, open(meta_file, 'rb') as m, open(prop_file, 'r') as pr:
+            payload_bytes = p.read()
+            meta_bytes = m.read()
+            props = json.load(pr)
+
+            # Determine action
+            action = determine_action(props["chunk_number"], props["number_chunks"])
+
+            # Add data
+            chunkID = await dfxapi.Measurements.add_data(session, measurement_id, props["chunk_number"], action,
+                                                         props["start_time_s"], props["end_time_s"],
+                                                         props["duration_s"], meta_bytes, payload_bytes)
+            print(f"Sent chunk id#:{chunkID} - {action} ...waiting {props['duration_s']:.0f} seconds...")
+
+            # Sleep to simulate a live measurement and not hit the rate limit
+            await asyncio.sleep(props["duration_s"])
+
+
+async def measure_websocket(session, measurement_id, measurement_files):
+    pass
+
 def cmdline():
     parser = argparse.ArgumentParser()
     parser.add_argument("--creds_file", default="./creds.json")
 
     subparser_top = parser.add_subparsers(dest="command", required=True)
     subparser_orgs = subparser_top.add_parser("org", help="Organizations").add_subparsers(dest="subcommand",
-                                                                                           required=True)
+                                                                                          required=True)
     register_parser = subparser_orgs.add_parser("register", help="Register device")
     register_parser.add_argument("license_key", help="DFX API Organization License")
     unregister_parser = subparser_orgs.add_parser("unregister", help="Unregister device")
@@ -227,6 +261,7 @@ def cmdline():
     make_parser = subparser_meas.add_parser("make", help="Make a measurement")
     make_parser.add_argument("study_id", help="Study ID to use", type=str)
     make_parser.add_argument("payloads_folder", help="Folder containing payloads", type=str)
+    make_parser.add_argument("--rest", help="Use REST instead of WebSocket (no results returned)", action="store_true")
     list_parser = subparser_meas.add_parser("list", help="List existing measurements")
     list_parser.add_argument("--limit", help="Number of measurements to retrieve", type=int, default=1)
     get_parser = subparser_meas.add_parser("get", help="Retrieve a measurement")
--- setup.py
+++ setup.py
@@ -7,9 +7,4 @@ setup(
     install_requires=['aiohttp[speedups]'],
     setup_requires=['wheel'],
     description='dfx-apiv2-client is an async client for the DeepAffex API.',
-    entry_points={
-        'console_scripts': [
-            'apiexample = apiexample:cmdline',
-        ],
-    },
 )
