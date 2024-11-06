2b3a1b609c47d5502eff0c3ad5149465a62cc549
Rajas Sambhare <sambhare@users.noreply.github.com>
2024-11-06 10:28:03 -0500

    Update setup to use build package

--- setup.py
+++ setup.py
@@ -10,12 +10,11 @@ setup(
     name='dfx-apiv2-client',
     author="NuraLogix Corporation",
     url='https://github.com/nuralogix/dfx-apiv2-client-py',
-    version='0.14.0',
+    version='0.15.0',
     packages=['dfx_apiv2_client'],
     install_requires=[
         'aiohttp<4',
     ],
-    setup_requires=['wheel'],
     description='dfx-apiv2-client is a Python 3 asyncio client library for the Nuralogix DeepAffex API.',
     long_description=long_description,
     long_description_content_type='text/markdown',
